bf00cffe058f24f10d95d48fca9b2fe8dbfb1459
bianekk <bianka.koww@gmail.com>
2024-04-23 18:45:16 +0200

    setting up directories

---
+++ src/facade-generator/imgtrans/img-translator.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class ImageTranslator(ABC):
+    """Abstract class for image translation models
+    
+    """
+    def __init__(self) -> None:
+        pass
+    
+    @abstractmethod
+    def forward(self):
+        """Run forward pass"""
+        pass
