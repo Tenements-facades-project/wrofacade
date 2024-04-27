13e23d0a1aa48620b75a844d327052dc672907ad
Hubert1225 <hubert.baran@omniscopy.com>
2024-04-27 22:54:09 +0200

    reformat code with black

--- src/facade_generator/facade_generator_abc.py
+++ src/facade_generator/facade_generator_abc.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 
 
-class FacadeGenerator():
-    """Class for FacadeGenerator. 
-    """
+class FacadeGenerator:
+    """Class for FacadeGenerator."""
+
     def __init__(self) -> None:
         pass
