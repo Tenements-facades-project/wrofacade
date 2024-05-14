0525e1c9deb702e3c549279b899bbac0b1b718c0
Hubert1225 <hubibaran@gmail.com>
2024-05-15 00:18:34 +0200

    continue script writing
    add creating parse trees and merging into one grammar

--- scripts/train_grammar_from_images.py
+++ scripts/train_grammar_from_images.py
@@ -3,23 +3,28 @@ import os
 import numpy as np
 import cv2
 from tqdm import tqdm
+from treelib import Tree
 
-from src.utils.grammars_utils.ascfg import Grammar
+from src.utils.grammars_utils.ascfg import Grammar, merge_grammars
 from src.utils.grammars_utils.bayesian_merging import BayesianMerger
 from src.utils.lattice_utils.data_manipulations import resize_facade, crop_facade
 from src.utils.lattice_utils.split_lines import infer_split_lines
 from src.utils.lattice_utils.lattice import Lattice
+from src.utils.grammars_utils.general_grammar import Facade, parse_facade, get_tree_loss
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("imgs_dir")
 parser.add_argument("masks_dir")
 parser.add_argument("output_dir")
+parser.add_argument("--window_labels", nargs="*", type=int, required=True)
 parser.add_argument("--output_pickle_name", default="grammar.pickle")
 parser.add_argument("--checkpoints_dir", default="checkpoints")
 parser.add_argument("--imgs_ext", default="png")
 parser.add_argument("--downsampling_factor", default=0.17, type=float)
 parser.add_argument("--background_label", default=0, type=int)
+parser.add_argument("--n_random_draws", type=int, default=100)
+parser.add_argument("--n_epochs", type=int, default=500)
 
 args = parser.parse_args()
 
@@ -63,6 +68,20 @@ def get_facade_lattice(
         horizontal_lines_inds=hor_lines_inds,
         vertical_lines_inds=ver_lines_inds,
     )
+
+
+def get_best_parse_tree(
+    lattice: Lattice
+) -> Tree:
+    facade_nt = Facade(
+        lattice=lattice,
+        max_ground_floor=0.3,
+        n_possible_splits=3,
+        window_labels=args.window_labels
+    )
+    trees = parse_facade(facade_nt, max_depth=-1)
+    trees_losses = [get_tree_loss(tree) for tree in trees]
+    return trees[np.argmin(trees_losses)]
 
 
 if __name__ == "__main__":
@@ -123,3 +142,28 @@ if __name__ == "__main__":
         )
         for img, mask in cropped_facades
     ]
+
+    # create parse trees from lattices
+
+    print("Creating parse trees of facades from lattices ...")
+    general_parse_trees = [
+        get_best_parse_tree(lattice)
+        for lattice in tqdm(lattices)
+    ]
+
+    # create ASCFG grammar from facades
+
+    print("Creating instance-specific grammars ...")
+    grammars = [
+        Grammar.from_general_parse_tree(tree)
+        for tree in tqdm(general_parse_trees)
+    ]
+    print("Merging into one stochastic grammar ...")
+    grammar = merge_grammars(grammars)
+
+    # induce generative grammar
+    merger = BayesianMerger(
+        w=2.0,
+        strategy="random_draw",
+        n_random_draws=args.n_random_draws,
+    )
