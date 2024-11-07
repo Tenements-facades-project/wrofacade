05ccb3704218d8ebd774aecf421235061867fd59
Hubert1225 <hubibaran@gmail.com>
2024-11-07 18:34:26 +0100

    add docstring in seg_mask.py

--- src/segmask/seg_mask.py
+++ src/segmask/seg_mask.py
@@ -2,9 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class SegMaskGenerator(ABC):
+    """Base class for all mask generators
+    (i.e. generates some segmentation mask).
+    Such a generated mask can be translated into
+    a new facade image
+
+    Note that mask generation can be parametrized,
+    and a parameter can be e.g. a facade image.
+    """
     def __init__(self) -> None:
         pass
 
     @abstractmethod
-    def generate_mask(self, args:dict):
+    def generate_mask(self, args: dict):
         pass
