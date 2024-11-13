744fc0b398dc8fccff15ac3b2e99713a36a72d35
Daniil Hardzetski <daniil.gordetsky@gmail.com>
2024-11-13 21:18:00 +0100

    Add StackGan util files

---
+++ src/utils/stack_gan_utils/__init__.py
@@ -0,0 +1,5 @@
+# source repo : https://github.com/rightlit/StackGAN-v2-rev
+# original project repo : https://github.com/hanzhanggit/StackGAN-v2
+
+
+from __future__ import division, print_function
